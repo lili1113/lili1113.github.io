f0f4e4b46f2ad6535953fcc54ea7a0b0ec648281
lili1113 <lili121wim@gmail.com>
2025-02-03 14:00:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// 添加上下文菜单
-function showContextMenu(e, node) {
-    const menu = document.createElement('div');
-    menu.style.position = 'fixed';
-    menu.style.left = `${e.clientX}px`;
-    menu.style.top = `${e.clientY}px`;
-    menu.innerHTML = `
-        <ul>
-            <li onclick="createNode(${node.id}, {x: ${e.clientX+50}, y: ${e.clientY+50}})">添加子节点</li>
-            <li onclick="deleteNode(${node.id})">删除节点</li>
-        </ul>
-    `;
-    document.body.appendChild(menu);
-    
-    setTimeout(() => menu.remove(), 2000);
-}
+
